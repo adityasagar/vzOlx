b2fb205933c014b294fdca8782adcb4d532386e1
adityasagar <adityasagar1992@gmail.com>
2015-08-30 01:06:19 -0700

    port

--- src/com/ProductUtility.java
+++ src/com/ProductUtility.java
@@ -68,7 +68,7 @@ public class ProductUtility {
 
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.port", "587");
 		props.put("mail.smtp.socketFactory.class",
 				"javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
